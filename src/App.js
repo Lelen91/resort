c2c388f9fff99bde543221fb3c114bc7264693fc
Lelen91 <30403621+Lelen91@users.noreply.github.com>
2019-09-19 00:36:07 +0100

    Set up router, add Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
-// import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <>Hello from app</>;
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/rooms/' component={Rooms} />
+        <Route exact path='/rooms/:slug' component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </React.Fragment>
+  );
 }
 
 export default App;
