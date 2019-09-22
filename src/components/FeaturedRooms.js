69136966c12277de0f022a3a69966a273ab542bb
Lelen91 <30403621+Lelen91@users.noreply.github.com>
2019-09-23 00:35:57 +0100

    add new components

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRooms: rooms } = this.context;
-    return <div>hello from featured rooms!</div>;
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
+    console.log("TCL: FeaturedRooms -> render -> rooms", rooms);
+    return (
+      <section className='featured-rooms'>
+        <Title title='Featured Rooms' />
+        <div className='featured-rooms-center'>
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import defaultBcg from "../images/room-1.jpeg";
 
-const SingleRoom = () => {
-  return <div>Hello from Single Room!</div>;
-};
+export default class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slug: this.props.match.params.slug,
+      defaultBcg
+    };
+  }
 
-export default SingleRoom;
+  static contextType = RoomContext;
+
+  // componentDidMount() {}
+
+  render() {
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className='error'>
+          <h3>No such room could be found...</h3>
+          <Link to='/rooms' className='btn-primary'>
+            Back to Rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+    } = room;
+
+    return (
+      <Hero className='rooms-hero'>
+        <Banner title={`${name} Room`}>
+          <Link to='/rooms' className='btn-primary'>
+            Back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
+  }
+}
