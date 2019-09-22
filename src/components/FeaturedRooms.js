4a150eaadeb1d4e186dbce7ca5e103a29f96101a
Lelen91 <30403621+Lelen91@users.noreply.github.com>
2019-09-22 16:50:02 +0100

    use context API in order to work with data.js, add Loading and FeaturedRooms components

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { RoomContext } from "../context";
+
+export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
+  render() {
+    const { featuredRooms: rooms } = this.context;
+    return <div>hello from featured rooms!</div>;
+  }
+}
